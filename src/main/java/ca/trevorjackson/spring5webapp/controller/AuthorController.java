1835cc46a0c9e0ea952024b36f04cbb987674049
Trevor Jackson <trevorlogin@gmail.com>
2017-12-02 20:08:11 -0800

    Section 2 Lecture 23 - Author Controller and template

---
+++ src/main/java/ca/trevorjackson/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,26 @@
+package ca.trevorjackson.spring5webapp.controller;
+
+import ca.trevorjackson.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Trevor Jackson
+ * December 02, 2017
+ **/
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
